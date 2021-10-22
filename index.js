cbbfebd0e84978e94f4f916c07612686b0837733
j-goldrath <jonathan.goldrath@gmail.com>
2021-10-22 14:58:42 -0500

    created all required directories and files, then added basic starter code to files

---
+++ index.js
@@ -0,0 +1,9 @@
+// Import required external modules
+const inquirer = require('inquirer');
+const fs = require('fs');
+
+// Import required local modules
+const Engineer = require('./lib/engineer')
+const Intern = require('./lib/intern')
+const Manager = require('./lib/manager')
+
