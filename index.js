abe154d6173b214d0d55d2aa4274f62fb1515886
j-goldrath <jonathan.goldrath@gmail.com>
2021-10-26 15:24:15 -0500

    most of code complete, still troubleshooting output file content generation

--- index.js
+++ index.js
@@ -147,7 +147,8 @@ function askWhatNext() {
             } else if (response.whatNext === 'Add an Intern') {
                 addIntern();
             } else {
-                console.log(newTeam);
+                console.log(newTeam);  // check contents of newTeam object array via console log
+                generateTeamProfilePage(newTeam);
             }
         })
         .catch((error) => {
@@ -160,4 +161,100 @@ function askWhatNext() {
 
 }
 
+// Generate team profile webpage file using employee objects generated while building team
+function generateTeamProfilePage(objArray) {
+
+    const pageTop = `<!DOCTYPE html>
+<html lang="en">
+    
+<head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <meta http-equiv="X-UA-Compatible" content="ie=edge" />
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.2/dist/css/bootstrap.min.css" rel="stylesheet"
+            integrity="sha384-uWxY/CJNBR+1zjPWmfnSnVxwRheevXITnMqoEIeG1LJrdI0GlVs/9cVSyPYXdcSF" crossorigin="anonymous">
+    <link rel="stylesheet" href="./style.css" />
+    <title>Team Profile Page</title>
+</head>
+    
+<body>
+    <nav class="navbar sticky-top navbar-light bg-danger text-white">
+        <div class="container justify-content-center">
+            <h1>My Team<h1>
+        </div>
+    </nav>
+
+    <div class="container mt-4">
+        <div class="row g-0 justify-content-center">
+    
+        `
+
+    fs.writeFile('./dist/team-profile-page.html', pageTop, err => {
+        if (err) {
+            console.error(err)
+            return
+        }
+        //file written successfully
+    });
+
+    objArray.forEach((employee) => {
+
+        let employeeSpecificInfo = '';
+
+        if (employee.constructor.name === 'Manager') {
+          employeeSpecificInfo = `Office Number: ${employee.officeNumber}`;
+        }
+        if (employee.constructor.name === 'Engineer') {
+          employeeSpecificInfo = `Github: https://github.com/${employee.github}`;
+        }
+        if (employee.constructor.name === 'Intern') {
+          employeeSpecificInfo = `School: ${employee.school}`;
+        }
+
+        const cardTemplate = `    <div class="card m-4" style="width: 18rem; filter: drop-shadow(0.35rem 0.35rem 0.4rem rgba(0, 0, 0, 0.5));">
+        <div class="card-header bg-primary text-white">
+            <h3>${employee.name}</h3>
+            <h4>${employee.constructor.name}</h4>
+        </div>
+        <div class="card-body">
+            <div class="card">
+                <ul class="list-group list-group-flush">
+                    <li class="list-group-item">ID: ${employee.id}</li>
+                    <li class="list-group-item">Email: ${employee.email}</li>
+                    <li class="list-group-item">${employeeSpecificInfo}</li>
+                </ul>
+            </div>
+        </div>
+    </div>
+
+    `
+
+    fs.appendFile('./dist/team-profile-page.html', cardTemplate, err => {
+        if (err) {
+            console.error(err)
+            return
+        }
+        //file written successfully
+    });
+
+    });
+
+    const pageBottom = `    </div>
+        </div>
+</body>
+
+</html>`
+
+    fs.appendFile('./dist/team-profile-page.html', pageBottom, err => {
+        if (err) {
+            console.error(err)
+            return
+        }
+        //file written successfully
+    });
+
+}
+
+
+// Start process by prompting user to add manager to the team
 addManager();
